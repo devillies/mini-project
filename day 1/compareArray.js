01d6eda88f9bbeed7737453c73785e2cd9eb840d
Juan <Me@magnolia.lan>
2018-11-09 15:00:39 +0700

    update to compareArray.js

--- day 1/compareArray.js
+++ day 1/compareArray.js
@@ -1,11 +1,3 @@
- let array = [1,2,3,4];
- let arrayCopy =  array
-console.log( array === arrayCopy);
-//mutating
- arrayCopy.push(5);
- console.log(arrayCopy);
- console.log(array === arrayCopy);
-// reassigning
- arrayCopy = [...array,5];
-console.log(arrayCopy);
-console.log(array === arrayCopy)
+let arrayOne = [1, 2, 3];
+let arrayTwo = [3, 2, 1];
+console.log(arrayOne.length === arrayTwo.length);
