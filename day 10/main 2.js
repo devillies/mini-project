6d94a133d19dd80263c1806c724277280c30a1e3
juan <juan@kodefox.com>
2018-12-05 16:21:43 +0700

    contact list and details

---
+++ day 10/main 2.js
@@ -0,0 +1,10 @@
+//@flow
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+let app = document.getElementById('app');
+
+if (app) {
+  ReactDOM.render(<App />, app);
+}
