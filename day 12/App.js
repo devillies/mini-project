acd353aa4d9000df08f9ce09a9695cd7ea926287
juan <juan@kodefox.com>
2018-12-19 18:31:31 +0700

    name and subs

--- day 12/App.js
+++ day 12/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
 function getApiUrl(onSuccess, onFail) {
   return new Promise((resolve, reject) => {
-    let response = fetch('https://api.github.com/orgs/citra-emu/repos');
-    response ? resolve(response) : reject('error');
+    let response = fetch(`https://api.github.com/users/citra-emu/repos`);
+    response ? resolve(response) : reject('error fetching');
   });
 }
 getApiUrl()
   .then((response) => {
     let answer = response.json();
     answer
-      .then((item) => item.map((elm) => console.log(elm.name)))
-      .catch((error) => console.log('cannot receive JSON'));
+      .then((citra) =>
+        citra.map((repo) => {
+          fetch(
+            `https://api.github.com/repos/citra-emu/${repo.name}/subscribers`,
+          )
+            .then((response) => response.json())
+            .then((sub) => console.log(`${repo.name}: ${sub.length}`));
+        }),
+      )
+      .catch((error) => console.log('There was an error:', error));
   })
   .catch((err) => console.log('there was an error: ' + err));
 export default getApiUrl;
