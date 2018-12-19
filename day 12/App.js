06f524ad8ce1c5df303af657093f02e91669f9d4
juan <juan@kodefox.com>
2018-12-19 14:36:59 +0700

    fix fetch

--- day 12/App.js
+++ day 12/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 function getApiUrl(onSuccess, onFail) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let response = fetch('https://api.github.com/orgs/citra-emu/repos');
-      response ? resolve(response) : reject('error');
-    }, 1000);
+    let response = fetch('https://api.github.com/orgs/citra-emu/repos');
+    response ? resolve(response) : reject('error');
   });
 }
 getApiUrl()
-  .then((response) => getApiUrl())
   .then((response) => {
     let answer = response.json();
-    console.log('success', answer);
-    return true;
+    answer
+      .then((jsonData) => {
+        console.log(jsonData);
+        let {id} = jsonData;
+        console.log(id);
+      })
+      .catch((error) => console.log('cannot receive JSON'));
   })
   .catch((err) => console.log('there was an error: ' + err));
 export default getApiUrl;
